5c2023e990610a41d35faa7892772147ee699216
YuYu1015 <whes1015@gmail.com>
2024-12-13 02:22:08 +0800

    fix: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,30 @@
 import BlogTimeline from "@/components/BlogTimeLine";
 import { Avatar } from "@mui/material";
-import * as React from "react";
+import React from "react";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="flex flex-col md:flex-row">
-      <div className="flex-[1] flex flex-col items-center pt-8">
-        <Avatar
-          className="m-3"
-          alt="Remy Sharp"
-          src="https://avatars.githubusercontent.com/u/44525760?v=4"
-          sx={{ width: 250, height: 250 }}
-        />
-        <span className="p-8 text-5xl font-black">whes1015</span>
-      </div>
-      <div className="flex-[4]">
-        <BlogTimeline />
+    <div className="container mx-auto px-4">
+      <div className="flex flex-col md:flex-row gap-8">
+        <aside className="md:w-1/4 flex flex-col items-center pt-8">
+          <Avatar
+            alt="whes1015"
+            src="https://avatars.githubusercontent.com/u/44525760?v=4"
+            sx={{
+              width: { xs: 200, md: 250 },
+              height: { xs: 200, md: 250 },
+              mb: 3,
+            }}
+          />
+          <h1 className="text-4xl md:text-5xl font-black">whes1015</h1>
+        </aside>
+
+        <main className="md:w-3/4">
+          <BlogTimeline />
+        </main>
       </div>
     </div>
   );
-}
+};
+
+export default Home;
